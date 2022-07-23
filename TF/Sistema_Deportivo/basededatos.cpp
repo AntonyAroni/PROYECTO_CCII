28f5f963efb3b2fb41cfa635b9f58042c4a561ed
AngelNifla <mnifla@unsa.edu.pe>
2022-07-22 21:26:42 -0500

    Fase 1 del nivel 1

---
+++ TF/Sistema_Deportivo/basededatos.cpp
@@ -0,0 +1,9 @@
+#include "basededatos.h"
+
+BaseDeDatos::BaseDeDatos(const QString &name_BD,
+                         const QString &name_Usua,
+                         const QString &pasword,
+                         int puerto_conexion)
+{
+    BD = QSqlDatabase::addDatabase("")
+}

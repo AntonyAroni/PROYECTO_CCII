c502f109e8a74d10d6998f5b0550e9fc86cf7070
JuanCarlosCastroBarreda <83053110+JuanCarlosCastroBarreda@users.noreply.github.com>
2022-07-22 21:32:15 -0500

    Add files via upload

---
+++ TF/build-Sistema_Deportivo-Desktop_Qt_5_12_12_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,76 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.12
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QAction *actionSalir;
+    QWidget *centralwidget;
+    QMenuBar *menubar;
+    QMenu *menuArchivo;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        actionSalir = new QAction(MainWindow);
+        actionSalir->setObjectName(QString::fromUtf8("actionSalir"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 20));
+        menuArchivo = new QMenu(menubar);
+        menuArchivo->setObjectName(QString::fromUtf8("menuArchivo"));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuArchivo->menuAction());
+        menuArchivo->addSeparator();
+        menuArchivo->addAction(actionSalir);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        actionSalir->setText(QApplication::translate("MainWindow", "Salir", nullptr));
+        menuArchivo->setTitle(QApplication::translate("MainWindow", "Archivo", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
